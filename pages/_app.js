5eacce6ce230497d99117d922490226fdd7ac4e9
irtiza-chowdhury <irtizachowdhury5@gmail.com>
2023-01-10 00:43:00 +0600

    Push the updates
    Code updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/globals.css'
+import Navbar from "../components/navbar";
+import "../styles/globals.css";
+import { Inter } from '@next/font/google'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+    <main className={inter.className}>
+      <Navbar />
+      <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
+
+export default MyApp;
---
+++ pages/api/basic.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from "node:fs";
+
+export default function handler(req, res) {
+  fs.readFile("blogdata/how-to-learn-flask.json", "utf-8", (err, data) => {
+    res.status(200).json(JSON.parse(data));
+  });
+}
